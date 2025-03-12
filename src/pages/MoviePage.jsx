001e47376b22bd7d1bdd896d45772cdf6f7dfda3
Gheorghe <madangheorghe.30@gmail.com>
2025-03-12 13:12:53 +0100

    Fetch movie details by ID using API in MoviePage component

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,8 +1,28 @@
-import { Link } from "react-router-dom";
+// import del Link e useParams dalla libreria react-router-dom
+import { Link, useParams } from "react-router-dom";
 
-// import dei componenti MovieCard e Review Card
-import MovieCard from "../components/MovieCard";
+// import del componente Review Card
 import ReviewCard from "../components/ReviewCard";
+
+// import del useState e useEffect
+import { useState, useEffect } from "react";
+
+// estrapolazione del parametro id dalla url per recuperare il film in base all'id
+const { id } = useParams();
+
+// Utilizzo di useState per salvare il film recuperato dalla chiamata API in get
+const [movie, setMovie] = useState({}); // inizializzazione dell'oggetto movie
+
+// Funzione per recuperare il film in base all'id effettuando una chiamata API con axios in get
+function getMovie() {
+    axios.get(`http://localhost:3000/api/movies/${id}`)
+        .then(res => setMovie(res.data))
+        .catch(err => console.error(err, 'Errore nel recupero del film'))
+}
+
+useEffect(() => getMovie(), []); // utilizzo di useEffect per chiamare la funzion getMovie al caricamento della pagina evitando il loop infinito
+
+
 
 // MoviePage.jsx
 export default function MoviePage() {
